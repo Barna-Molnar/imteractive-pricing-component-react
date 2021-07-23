40033acc324af7af8b4250e8ac5eb885552c2162
Barnabas Molnar <hanta911@gmail.com>
2021-07-23 20:31:36 +0200

    getting Started

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const AppStlyes = styled.div`
+  background-color: var(--main-background);
+  width: 100vw;
+  height: 100vh;
+`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <AppStlyes>
+      <h1>hell from the main app </h1>
+    </AppStlyes>
+  );
 }
 
 export default App;
