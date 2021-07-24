d56c0618601e495a6050d8e7806f1452e29fe669
Barnabas Molnar <hanta911@gmail.com>
2021-07-24 21:12:40 +0200

    Title style Stage1

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,25 +3,37 @@ import styled from 'styled-components';
 import BackPattern from '../images/pattern-circles.svg';
 
 const TitleStyles = styled.div`
-  background: url(${BackPattern}) no-repeat center;
+  /* background: url(${BackPattern}) no-repeat center; */
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 5rem 4rem;
+  padding: 4rem 4rem;
+  border: solid black 1px;
+  position: relative;
+  z-index: 0;
+  img {
+    width: 120px;
+    z-index: 1;
+    position: absolute;
+  }
   h1 {
     font-size: 2.2rem;
     color: var(--dark-blue);
+    margin-bottom: 0.5rem;
+    z-index: 2;
   }
   p {
     font-size: 12px;
     color: var(--grayish-blue-text);
+    z-index: 2;
   }
 `;
 
 export default function Title() {
   return (
     <TitleStyles>
+      <img src={BackPattern} alt="" />
       <h1>Simple, traffic-based pricing</h1>
       <p>Sign-up for our 30-day trial. No credit card required. </p>
     </TitleStyles>
