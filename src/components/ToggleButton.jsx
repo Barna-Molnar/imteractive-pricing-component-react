c1b69742b62f2f3336a7a7fbf34b72a7af79d9d3
Barnabas Molnar <hanta911@gmail.com>
2021-07-26 18:28:27 +0200

    Creating custom toggleButton

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -2,55 +2,42 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ToggleButtonStyles = styled.div`
-  .switch__container {
-    height: 40px;
-    text-align: center;
+  input[type='checkbox'] {
+    position: relative;
+    width: 4rem;
+    height: 1.8rem;
+    -webkit-appearance: none;
+    background: var(--toggle-background);
+    outline: none;
+    transition: 0.5s;
+    border-radius: 10px;
+    box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
+  }
+  input:checked[type='checkbox'] {
+    background: var(--soft-cyan);
+  }
 
-    /** Switch
- -------------------------------------*/
-
-    .switch input {
-      position: absolute;
-      opacity: 0;
-    }
-    .switch {
-      display: flex;
-      align-items: center;
-      font-size: 20px; /* 1 */
-      height: 2rem;
-      width: 4rem;
-      background: var(--toggle-background);
-      border-radius: 1em;
-      /*   padding: 0.25rem; */
-    }
-
-    .switch div {
-      height: 1.5rem;
-      width: 1.5rem;
-      border-radius: 1.5rem;
-      background: #fff;
-      box-shadow: 0 0.1em 0.3em rgba(0, 0, 0, 0.3);
-      -webkit-transition: all 300ms;
-      -moz-transition: all 300ms;
-      transition: all 300ms;
-    }
-
-    .switch input:checked + div {
-      -webkit-transform: translate3d(150%, 0, 0);
-      -moz-transform: translate3d(150%, 0, 0);
-      transform: translate3d(150%, 0, 0);
-    }
+  input[type='checkbox']:before {
+    content: '';
+    position: absolute;
+    left: 2px;
+    top: 3px;
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    background: #fff;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+    transition: 0.5s;
+  }
+  input:checked[type='checkbox']:before {
+    left: 2.6rem;
   }
 `;
 
 export default function ToggleButton() {
   return (
     <ToggleButtonStyles>
-      <div class="switch__container">
-        <label class="switch">
-          <input type="checkbox" /> <div></div>
-        </label>
-      </div>
+      <input type="checkbox" />
     </ToggleButtonStyles>
   );
 }
